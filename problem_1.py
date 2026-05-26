046506d6c07af45147cf13158f635a6390456aa7
SID <65087687+ss-deshmukh@users.noreply.github.com>
2026-05-26 11:49:32 +0530

    New task

---
+++ problem_1.py
@@ -0,0 +1,10 @@
+# Solve the following problem:
+# Write a function to calculate the number of seconds in a week
+
+# Write code here
+
+def seconds_in_week():
+
+  return(result)
+
+print(seconds_in_week)
