cf3705c52f0aa55d07df4cab2b65576994dbb688
ayushkamutdak-creator <ayushkamutdak@gmail.com>
2026-06-02 11:36:14 +0530

    Update 38-aayushka -mutdak.py

--- lab/38-aayushka -mutdak.py
+++ lab/38-aayushka -mutdak.py
@@ -4,3 +4,16 @@ while n=<10:
   t+=n
   n+=1
 print(t)
+#problem
+a=4
+b=9
+c=19
+def check(n)
+     if(n%2=0)
+        print("even")
+     else:
+        print("odd")
+
+check(a)
+check(b)
+check(c)
