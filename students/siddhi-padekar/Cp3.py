e401827d94b7c6d5c8a81bad1e77c554c84fa3a1
39_siddhi padelar <siddhipadekar2@gmail.com>
2026-06-18 11:28:47 +0530

    Cp3.py

---
+++ students/siddhi-padekar/Cp3.py
@@ -0,0 +1,13 @@
+#problem1
+def collect_all_sub():
+    sub = []
+
+    ele = collect_one_sub()
+    subs. append(ele)
+
+    while len(susb) <= MAX_SUB:
+        if ask_yes_no("Add another sub "):
+          ele  = collect_one_sub()
+          subs. applend
+        else:
+            break
